72470a7d349e10e27bffa2c45aeb0a69e230bff2
Gleidson <gleidsonrosasantos2@gmail.com>
2026-06-22 14:39:53 -0300

    js preparado para desafio

---
+++ js/scripts_pessoas.js
@@ -0,0 +1,24 @@
+//PEGANDO ELEMENTOS DO DOM
+const formPessoa = document.querySelector('#form-pessoa')
+const divLista = document.querySelector('#div-lista-pessoas')
+
+//CRIANDO O ARRAY PESSOAS
+const pessoa = []
+
+
+//CAPITURAR O EVENTO submit DO FORMULARIO
+formPessoa.addEventListener('submit', (evt) => {
+    //INTERROMPER O EFEITO PADRÃO DE SUBMETER OS DADOS DO FORMULARIO
+    evt.preventDefault()
+
+    //CRIAR UM OBJETO FORMULARIO
+    const dadosFormPessoa = new FormData(formPessoa)
+
+    //CRIAR UM OBJETO LITERAL
+    const pessoa = {
+        nome: dadosFormPessoa.get('nome'),
+        idade: dadosFormPessoa.get('idade'),
+        renda: dadosFormPessoa.get('renda')
+    }
+
+})
