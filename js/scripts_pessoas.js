ff3cc3fedead8d18bf4c90722baeeda414df6a9d
Gleidson <gleidsonrosasantos2@gmail.com>
2026-06-22 15:01:13 -0300

    js finalizado

--- js/scripts_pessoas.js
+++ js/scripts_pessoas.js
@@ -21,9 +21,21 @@ formPessoa.addEventListener('submit', (evt) => {
         renda: dadosFormPessoa.get('renda')
     }
 
+    //CHAMANDO A FUNÇÃO addPessoa E PASSANDO O OBJETO LITERAL pessoa
+    addPessoa(pessoa)
+
 })
 
 //CRIANDO A FUNÇÃO ADICIONAR PESSOA 
 const addPessoa = (objPessoa) => {
+    //ADICIONANDO O OBJETO LITERAL NO ARRAY PESSOAS
     pessoas.push(objPessoa)
 }
+
+//FUNÇÃO PARA LISTAR PESSOAS DO ARRAY
+const listPessoas = () => {
+    pessoas.forEach((elem, i) => {
+        divLista.innerHTML += `${i} - ${elem.nome} - ${elem.idade} - ${elem.renda} <br>`
+    })
+}
+
